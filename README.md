517e2bc46a4943e19acb465e7c078c6120810e8e
administator <administator>
2016-03-26 11:32:16 +0800

    增加模拟数据测试异步请求

--- README.md
+++ README.md
@@ -14,3 +14,17 @@ toggle 开关按钮 通过input的checked属性查看状态
 ###工具栏切换页面
 使用external，刷新页面进入新的链接  
 不同的工具栏对应不同的SPA链路的入口
+
+###创建json文件测试ajax请求
+$.post请求不到tpl下的json文件 
+json文件的格式一定要写正确，否则会出现错误，比如：  
+$.get请求的到，但不执行回调
+fetch请求的到，且可以获取到解析json出错的信息  
+
+###SUI基本组件使用方法
+1, 轮播图组件swiper，使用data-xxx把相应的属性设置在轮播容器上，  
+   当页面中有多个轮播图时，data-pagination=".first-pagination"来设置轮播图相对应的分页部分  
+2, 轻量级提示组件toast  
+   $.toast(msg, staytime, class) eg: $.toast("添加成功", 3000, "success")  
+   其中样式需要在样式表中自定义
+3, popup,
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,26 @@
 $(function () {
 	//初始化页面，必须执行
 	$.init();
+
+	//添加阅读记录弹出层效果
+	$(document).on('click', '.open-newnote', function () {
+		$.popup('.popup-newnote');
+	});
+
+	//确定添加阅读记录
+	$('.save-note').on('click', function () {
+		//表单校验
+		$.get("../tpl/save-note.json", function (res) {
+			$.toast(res.msg, 2000, "toaststyle");
+			$.closeModal('.popup-newnote');
+		});
+		//使用fetch方法请求数据
+		// fetch("tpl/save-note.json").then(function (res) {
+		// 	return res.json()
+		// }).then (function (json) {
+		// 	console.log('parsed json', json)
+		// }).catch(function (ex) {
+		// 	console.log('parsing failed', ex)
+		// });
+	});
 }) 
